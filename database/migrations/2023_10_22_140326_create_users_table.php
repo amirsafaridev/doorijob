4818beb4b10e4dacfd64df942535ec7a76ab4173
malifathi <32976447+malifathi@users.noreply.github.com>
2023-10-25 15:05:13 +0330

    first attend commit

--- database/migrations/2023_10_22_140326_create_users_table.php
+++ database/migrations/2023_10_22_140326_create_users_table.php
@@ -13,11 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            
+            /* Add additional fields */
+            $table->text('user_name');  
+            $table->text('password');  
+            $table->text('first_name');   
+            $table->text('last_name'); 
+            $table->text('role'); 
+            $table->text('phone_number'); 
+          
+            
             $table->timestamps();
         });
     }
